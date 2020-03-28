92c2e1df16e29dbbe4b6b373c5e98425b69f50af
eddydesh <62362273+eddydesh@users.noreply.github.com>
2020-03-28 16:08:45 +0530

    Create Objects.py

---
+++ Objects.py
@@ -0,0 +1,9 @@
+monitor =  "Dell Alienware AW3418DW"
+keyboard = "Dell Alienware AW768"
+mouse = "Dell Alienware AW959"
+cabinet = "Dell Alienware Aurora"
+
+yourname = "Smart-Employee"
+
+print("Hello ", yourname, ",", "\nYou have really amazing system with this amazing configuration:")
+print("\n", cabinet, "\n", monitor, "\n", keyboard, "\n", mouse)
